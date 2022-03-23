864d7f666a23878dc571073273e99c50334a4bc9
Hellogooys <99305409+Hellogooys@users.noreply.github.com>
2022-03-23 13:42:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('♻️ Group', url='https://t.me/+2ZiffS53U-43YTJl')
+                InlineKeyboardButton('🍿 Group 🍿', url='https://t.me/+2ZiffS53U-43YTJl')
             ],
             [
                 InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
@@ -45,10 +45,10 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('♻️ Group', url='https://t.me/+2ZiffS53U-43YTJl')
+            InlineKeyboardButton('🍿 Group 🍿', url='https://t.me/+2ZiffS53U-43YTJl')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('🧑🏽 About', callback_data='about')
+            InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -88,10 +88,10 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('♻️ Group', url='https://t.me/+2ZiffS53U-43YTJl')
+            InlineKeyboardButton('🍿 Group 🍿', url='https://t.me/+2ZiffS53U-43YTJl')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('🧑🏽 About', callback_data='about')
+            InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
