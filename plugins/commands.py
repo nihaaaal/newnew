fa4dc849bf2ea21bf667b53b32b43727f5f75c07
Hellogooys <99305409+Hellogooys@users.noreply.github.com>
2022-03-23 13:54:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('🍿 Group 🍿', url='https://t.me/+2ZiffS53U-43YTJl')
+                InlineKeyboardButton('🍿 Group 🍿', url='https://t.me/movie_lookam')
             ],
             [
                 InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
@@ -45,7 +45,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🍿 Group 🍿', url='https://t.me/+2ZiffS53U-43YTJl')
+            InlineKeyboardButton('🍿 Group 🍿', url='https://t.me/movie_lookam')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -88,7 +88,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🍿 Group 🍿', url='https://t.me/+2ZiffS53U-43YTJl')
+            InlineKeyboardButton('🍿 Group 🍿', url='https://t.me/movie_lookam')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
