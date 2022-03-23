9c0944fbce50320da7bf3d49198d5a2da8271b74
Hellogooys <99305409+Hellogooys@users.noreply.github.com>
2022-03-23 09:24:33 +0630

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -8,7 +8,7 @@ from utils import get_size, temp, get_settings
 from Script import script
 from pyrogram.errors import ChatAdminRequired
 
-"""-----------------------------------------https://t.me/GetTGLink/4179 --------------------------------------"""
+"""-----------------------------------------https://t.me/NL_MP4 --------------------------------------"""
 
 @Client.on_message(filters.new_chat_members & filters.group)
 async def save_group(bot, message):
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('♻️ Group', url='https://t.me/+2ZiffS53U-43YTJl')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
