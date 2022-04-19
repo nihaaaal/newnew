a63de6d73d125deb236e2d1dcf1524aed86a4b67
Hellogooys <99305409+Hellogooys@users.noreply.github.com>
2022-04-19 17:40:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -64,7 +64,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[🔮 {get_size(file.file_size)} 🔮][🎪 {file.file_name} 🎪", callback_data=f'files#{file.file_id}'
+                    text=f"[🔮 {get_size(file.file_size)} 🎪 {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -655,7 +655,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🔮 {get_size(file.file_size)}]🎪 {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"🔮 {get_size(file.file_size)}🎪 {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
