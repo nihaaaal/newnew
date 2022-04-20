df49818a7ba1fac2ffa9a74a3d1074baf5100f9f
Hellogooys <99305409+Hellogooys@users.noreply.github.com>
2022-04-20 12:59:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -398,7 +398,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 f_caption = f_caption
         if f_caption is None:
             f_caption = f"{title}"
-          buttons = [
+        buttons = [
               [
                   InlineKeyboardButton('🎬 Series & Movie Club 🎬', url=f'https://t.me/movie_lookam')
               ]
