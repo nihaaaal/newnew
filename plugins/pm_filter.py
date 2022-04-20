d265a7587f9a28e8654ee28ef5660e141c090557
Hellogooys <99305409+Hellogooys@users.noreply.github.com>
2022-04-20 13:10:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -404,11 +404,19 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]
             ]
         await query.answer()
+
         await client.send_cached_media(
+
             chat_id=query.from_user.id,
+
             file_id=file_id,
+
             caption=f_caption,
+
+            reply_markup=InlineKeyboardMarkup(buttons),
+
             protect_content=True if ident == 'checksubp' else False
+
         )
     elif query.data == "pages":
         await query.answer()
