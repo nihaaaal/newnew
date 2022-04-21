b2b24ddae7dafb5dbdc4574f28450dac97b749f8
Hellogooys <99305409+Hellogooys@users.noreply.github.com>
2022-04-21 16:23:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -688,6 +688,10 @@ async def auto_filter(client, msg, spoll=False):
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
         )
+        btn.insert(0, [
+        InlineKeyboardButton("🎀 𝙹𝙾𝙸𝙽 𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 🎀",url="https://t.me/movie_lookam")
+
+        ])
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
     if imdb:
